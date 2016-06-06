8dccd8b877e94757607f6bbf9b149a1e1da04c3f
e0eaea <alsrb72@naver.com>
2016-06-06 09:22:36 +0000

    dynamoDB scanning success

--- COS_Server/mkcloud/routes/index.js
+++ COS_Server/mkcloud/routes/index.js
@@ -1,29 +1,3 @@
-/**
- * Created by AvILoS on 7/28/15.
- */
-var schedule = require('node-schedule');        // Node 스케줄러
-var time = require('time');                     // Time
-
-var path = process.cwd();
-
-var logger = require(path+'/config/logger.js');        // Winston Logger
-
-var timezone = time.currentTimeZone;        // Asia/Seoul
-
-logger.info(timezone);
-
-
-// 매시간 1분마다 한번씩 수행
-var rule1 = new schedule.RecurrenceRule();
-rule1.minute = 1;
-var scheduledJob2 = schedule.scheduleJob(rule1,
-    function(){
-        logger.info('매시간 1분마다 수행!!');
-	console.log('매시간 1분마다 수행!!');
-    }
-);
-
-
 var express = require('express');
 
 var AWS = require('aws-sdk');
@@ -35,6 +9,59 @@ var multer = require("multer");             // 파일 업로드 모듈
 var upload = multer({ dest: 'upload/' });   // 파일 업로드 경로 설정
 var db = new AWS.DynamoDB.DocumentClient();
 
+
+var schedule = require('node-schedule');        // Node 스케줄러
+var time = require('time');                     // Time
+
+var path = process.cwd();
+
+var logger = require(path+'/config/logger.js');        // Winston Logger
+
+var timezone = time.currentTimeZone;        // Asia/Seoul
+
+
+
+
+logger.info(timezone);
+
+
+// 30초마다 한번씩 수행
+var rule1 = new schedule.RecurrenceRule();
+rule1.second = 30;
+var scheduledJob2 = schedule.scheduleJob(rule1,
+    function(){
+        logger.info('30초마다 수행!!');
+	console.log('30마다 수행!!');
+
+var params = {
+    TableName: "mkcloud-dynamo"
+};
+
+console.log("Scanning Movies table.");
+db.scan(params, onScan);
+
+function onScan(err, data) {
+    if (err) {
+        console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        // print all the movies
+        console.log("Scan succeeded.");
+	data.Items.forEach(function(item) {
+           console.log(item);
+        });
+      
+        // continue scanning if we have more movies
+        if (typeof data.LastEvaluatedKey != "undefined") {
+            console.log("Scanning for more...");
+            params.ExclusiveStartKey = data.LastEvaluatedKey;
+            db.scan(params, onScan);
+        }
+    }
+}
+
+});
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'fileUpload Example' });
@@ -43,24 +70,27 @@ router.get('/', function(req, res, next) {
 
 // upload.single('var') : var 에는 file name 을 적어주어야 한다.
 router.post('/upload', upload.single('uploadFile'), function (req, res, next) {
-  console.log(req.body.UserData);
- var userName=req.body.UserData;
-//  console.log(req.file);
+
+ var user_data = JSON.parse(req.body.json);
+
+  var userName=user_data.id;
+  var due_date=user_data.date;
+  var filename=user_data.image_name;
 
   var tmp_path = req.file.path;                                             // 원래 파일 경로
-  var target_path = 'upload/' + Date.now() + "_" + req.file.originalname;   // 새로 저장할 파일 경로
+  var target_path = 'upload/' + Date.now() + "_" + filename;   // 새로 저장할 파일 경로
 
   //var src = fs.createReadStream(tmp_path);        // 원래 파일을 스트림으로 읽기
  // var dest = fs.createWriteStream();   // 새로 저장할 파일을 스트림으로 쓰기
 
 
  var body = fs.createReadStream(tmp_path);
- var s3obj = new AWS.S3({params: {Bucket: 'mkcloud',Key: userName+'/' +req.file.originalname}});
+ var s3obj = new AWS.S3({params: {Bucket: 'mkcloud',Key: userName+'/' +filename}});
  s3obj.upload({Body: body}).
   on('httpUploadProgress', function(evt) { console.log(evt); }).
   send(function(err, data) { 
 	var params  = { TableName:'mkcloud-dynamo',
-			Item:{  id:userName, url:data.Location   }};
+			Item:{  id:userName, url:data.Location, due_date:due_date  }};
  	 db.put(params, function(err,data) {
 	 console.log('업로드성공');
 	}); 
